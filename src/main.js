2dc2326cf1586f8d78077d9641b9469a4c9a0d18
Ilfat Mindubaev <yagarantuz@gmail.com>
2019-03-11 01:13:14 +0300

    Tweak styles & add ui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
+import VueClipboard from 'vue-clipboard2'
 import 'vue-material/dist/vue-material.css'
 import App from './App.vue'
 
 Vue.use(VueMaterial);
+Vue.use(VueClipboard);
 
 new Vue({
   el: '#app',
